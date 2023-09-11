d7eca24c3531196c8f5e7abacb9190f46d9b4700
maruf <dev.maruf112@gmail.com>
2023-09-11 15:53:06 +0600

    Zod Validation done

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,26 +1,19 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemester.service';
 import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemester } from '@prisma/client';
 import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
 
-const insertIntoDb = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const result = await AcademicSemesterService.insertIntoDb(req.body);
-    sendResponse<AcademicSemester>(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Academic Semester created successfully.',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const insertIntoDb = catchAsync(async (req: Request, res: Response) => {
+  const result = await AcademicSemesterService.insertIntoDb(req.body);
+  sendResponse<AcademicSemester>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Semester created successfully.',
+    data: result,
+  });
+});
 export const AcademicSemesterController = {
   insertIntoDb,
 };
--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,5 +1,11 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 const router = express.Router();
-router.post('/', AcademicSemesterController.insertIntoDb);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDb
+);
 export const AcademicSemesterRoutes = router;
