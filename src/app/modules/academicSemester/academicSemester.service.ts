eea03fa3a5e7d7b452584f575e894302a93d783b
maruf <dev.maruf112@gmail.com>
2023-09-12 08:52:51 +0600

    dynamically done searchable fields

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,4 +1,4 @@
-import { AcademicSemester, PrismaClient } from '@prisma/client';
+import { AcademicSemester, Prisma, PrismaClient } from '@prisma/client';
 import { IGenericResponse } from '../../../interfaces/common';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IPaginationOptions } from '../../../interfaces/pagination';
@@ -20,23 +20,20 @@ const getAllFromDb = async (
 ): Promise<IGenericResponse<AcademicSemester[]>> => {
   const { page, skip, limit } = paginationHelpers.calculatePagination(options);
   const { searchTerm, ...filterData } = filters;
+  const andConditions = [];
+  if (searchTerm) {
+    andConditions.push({
+      OR: ['title', 'code', 'startMonth', 'endMonth'].map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });}
+  const whereConditions: Prisma.AcademicSemesterWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
   const result = await prisma.academicSemester.findMany({
-    where: {
-      OR: [
-        {
-          title: {
-            contains: searchTerm,
-            mode: 'insensitive',
-          },
-        },
-        {
-          code: {
-            contains: searchTerm,
-            mode: 'insensitive',
-          },
-        },
-      ],
-    },
+    where: whereConditions,
     skip,
     take: limit,
   });
