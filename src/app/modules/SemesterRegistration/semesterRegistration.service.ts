757b76d293a1118737fdf312b98a24649e1478cc
maruf <dev.maruf112@gmail.com>
2023-09-16 22:48:09 +0600

    handle ongoing or upcoming registration

--- src/app/modules/SemesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/SemesterRegistration/semesterRegistration.service.ts
@@ -1,9 +1,33 @@
-import { SemesterRegistration } from '@prisma/client';
+import {
+  SemesterRegistration,
+  SemesterRegistrationStatus,
+} from '@prisma/client';
 import { prisma } from '../../../shared/prisma';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
 
 const insertIntoDb = async (
   data: SemesterRegistration
 ): Promise<SemesterRegistration> => {
+  const isSemesterOngoingOrUpcoming =
+    await prisma.semesterRegistration.findFirst({
+      where: {
+        OR: [
+          {
+            status: SemesterRegistrationStatus.UPCOMING,
+          },
+          {
+            status: SemesterRegistrationStatus.ONGOING,
+          },
+        ],
+      },
+    });
+  if (isSemesterOngoingOrUpcoming) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      `Already and ${isSemesterOngoingOrUpcoming.status} registration.`
+    );
+  }
   const result = await prisma.semesterRegistration.create({
     data,
   });
