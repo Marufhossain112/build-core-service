2c8fe3aa3c5e3ece00bd1272368d3d4e8d042775
maruf <dev.maruf112@gmail.com>
2023-09-17 04:56:48 +0600

    Control upcoming to ongoing and ongoing to ended

--- src/app/modules/SemesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/SemesterRegistration/semesterRegistration.service.ts
@@ -37,6 +37,38 @@ const updateToDb = async (
   id: string,
   payload: Partial<SemesterRegistration>
 ) => {
+  // console.log(payload.status);
+  const isExist = await prisma.semesterRegistration.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!isExist) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Semester registration does not exist.'
+    );
+  }
+  if (
+    payload.status &&
+    isExist.status === SemesterRegistrationStatus.UPCOMING &&
+    payload.status !== SemesterRegistrationStatus.ONGOING
+  ) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Can only move from upcoming to ongoing.'
+    );
+  }
+  if (
+    payload.status &&
+    isExist.status === SemesterRegistrationStatus.ONGOING &&
+    payload.status !== SemesterRegistrationStatus.ENDED
+  ) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Can only move from ongoing to ended.'
+    );
+  }
   const result = await prisma.semesterRegistration.update({
     where: { id },
     data: payload,
