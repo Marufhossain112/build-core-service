b76281c6b391d4be726cb3a6925a18d0d5d822f0
maruf <dev.maruf112@gmail.com>
2023-09-17 05:07:03 +0600

    zod validation update for semester registration

--- src/app/modules/SemesterRegistration/semesterRegistration.validation.ts
+++ src/app/modules/SemesterRegistration/semesterRegistration.validation.ts
@@ -1,3 +1,4 @@
+import { SemesterRegistrationStatus } from '@prisma/client';
 import { z } from 'zod';
 
 const createSemesterRegistrationValidation = z.object({
@@ -11,14 +12,16 @@ const createSemesterRegistrationValidation = z.object({
 });
 const updateSemesterRegistrationValidation = z.object({
   body: z.object({
-    startDate: z
-      .string({ required_error: 'startDate is required.' })
-      .optional(),
-    endDate: z.string({ required_error: 'endDate is required.' }).optional(),
-    academicSemesterId: z
-      .string({
-        required_error: 'academicSemesterId is required.',
-      })
+    startDate: z.string().optional(),
+    endDate: z.string().optional(),
+    academicSemesterId: z.string().optional(),
+    minCredit: z.number().optional(),
+    maxCredit: z.number().optional(),
+    status: z
+      .enum([...Object.values(SemesterRegistrationStatus)] as [
+        string,
+        ...string[]
+      ])
       .optional(),
   }),
 });
