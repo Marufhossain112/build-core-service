73dfb35272b7c998b04d68dbe063ef9738da0755
maruf <dev.maruf112@gmail.com>
2023-09-26 09:26:16 +0600

    mark updated

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.service.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.service.ts
@@ -1,5 +1,8 @@
 import { ExamType, PrismaClient } from "@prisma/client";
 import { DefaultArgs, PrismaClientOptions } from "@prisma/client/runtime/library";
+import { prisma } from "../../../shared/prisma";
+import ApiError from "../../../errors/ApiError";
+import httpStatus from "http-status";
 
 const createStudentEnrolledCourseDefaultMark = async (
     prismaClient: Omit<PrismaClient<PrismaClientOptions, never, DefaultArgs>, "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends">, payload: {
@@ -82,8 +85,52 @@ const createStudentEnrolledCourseDefaultMark = async (
     }
 };
 const updateStudentMarks = async (payload: any) => {
-    console.log(payload);
+    // console.log(payload);
+    const { studentId, academicSemesterId, examType, marks, courseId } = payload;
+    let grade = '';
+    const studentEnrolledCourseMarks = await prisma.studentEnrolledCourseMark.findFirst({
+        where: {
+            student: {
+                id: studentId
+            },
+            academicSemester: {
+                id: academicSemesterId
+            }, studentEnrolledCourse: {
+                course: { id: courseId }
+            }, examType
+        }
+    });
+    if (!studentEnrolledCourseMarks) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "Student enrolled marks not found.");
+    }
+    if (marks >= 0 && marks <= 39) {
+        grade = "F";
+    }
+    else if (marks >= 40 && marks <= 49) {
+        grade = "D";
+    }
+    else if (marks >= 50 && marks <= 59) {
+        grade = "C";
+    }
+    else if (marks >= 60 && marks <= 69) {
+        grade = "B";
+    }
+    else if (marks >= 70 && marks <= 79) {
+        grade = "A";
+    }
+    else if (marks >= 80 && marks <= 100) {
+        grade = "A+";
+    }
+    const updateStudentMarks = await prisma.studentEnrolledCourseMark.update({
+        where: {
+            id: studentEnrolledCourseMarks.id
+        }, data: {
+            marks, grade
+        }
+    });
+    return updateStudentMarks;
 };
+
 export const StudentEnrolledCourseMarkService = {
     createStudentEnrolledCourseDefaultMark,
     updateStudentMarks
