fac022e46f35c14091b292cfa2cc03793b702ea6
maruf <dev.maruf112@gmail.com>
2023-09-26 08:53:37 +0600

    cloned the hw part of 42

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes.ts
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { StudentEnrolledCourseMarkController } from './studentEnrolledCourseMark.controller';
-const router = express.Router();
-router.patch("/update-mark", StudentEnrolledCourseMarkController.updateStudentMarks);
-export const StudentEnrolledCourseMarkRoutes = router;
